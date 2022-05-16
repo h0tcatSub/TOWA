dc34a598494ea2e99cc502859f26ce6427f73127
h0tcatSub <h0tcatnohotmensh0tcat@gmail.com>
2022-05-16 21:03:44 +0900

    [WIP] うねうね移動

--- core/src/h0tcat/game/towa/ui/Cursor.java
+++ core/src/h0tcat/game/towa/ui/Cursor.java
@@ -1,5 +1,7 @@
 package h0tcat.game.towa.ui;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -11,13 +13,17 @@ public class Cursor extends Sprite{
     private SpriteBatch batch;
     private Sprite sprite;
 
-    private float x, y;
-    
+    private float x, y, waveX, waveY;
+    private double rY;
+    private double rX;
+    private double deg = 0;
+    private int index;
+
     public Cursor(FileHandle handle){
         texture = new Texture(handle);
         sprite = new Sprite(texture);
         batch = new SpriteBatch();
-        
+        index = 0;
         super.setTexture(texture);
         sprite.setScale(0.3f, 0.3f);
     }
@@ -27,33 +33,45 @@ public class Cursor extends Sprite{
 
         this.x = x;
         this.y = y;
-        move(x, y);
+        rX = 0.7;
+        rY = 0.01;
     }
 
     public Cursor(FileHandle handle, float x, float y, float originX, float originY){
         this(handle, x, y);
         super.setOrigin(originX, originY);
-
-        this.x = x;
-        this.y = y;
     }
 
 
     public float getX(){
-        return x;
+        return (float) x;
     }
     public float getY(){
-        return y;
+        return (float) y;
     }
 
     public void move(float x, float y) {
         this.x = x;
         this.y = y;
-        sprite.translate(x, y);
     }
 
     public void draw(){
+        deg += 0.1;
+        if(deg >= Double.MAX_VALUE - 100){
+            deg = 0;
+        }
+        waveX = (float) (rX * Math.cos(deg));
+        waveY = (float) (rY * Math.sin(deg));
         batch.begin();
+
+        if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+            y += 29;
+        } else if (Gdx.input.isKeyPressed(Keys.UP)) {
+            y -= 29;
+        }
+
+        sprite.setY(y);
+        sprite.translate((float)waveX, (float)waveY);
         sprite.draw(batch);
         batch.end();
     }
