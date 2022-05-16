b0c6453fd212ad6b5932c9f977322d02a30ce6bd
h0tcatSub <h0tcatnohotmensh0tcat@gmail.com>
2022-05-16 21:05:08 +0900

    [WIP] first commit

--- core/src/h0tcat/game/towa/util/GameSound.java
+++ core/src/h0tcat/game/towa/util/GameSound.java
@@ -3,12 +3,23 @@ package h0tcat.game.towa.util;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
-public class GameSound{
+public class GameSound {
     private final String soundDirectory = "data/syssnd/";
     private Music sound;
 
     public GameSound(String soundName){
         sound = Gdx.audio.newMusic(Gdx.files.internal(soundDirectory.concat(soundName)));
-        
+    }
+
+    public boolean play(){
+        sound.play();
+        return sound.isPlaying();
+    }
+    public boolean loop(){
+        if(sound == null){
+            return false;
+        }
+        sound.setLooping(true);
+        return true;
     }
 }
