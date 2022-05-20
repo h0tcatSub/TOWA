6cabfebb826979210e9adde3ffbbdf85c5ed54e3
h0tcatSub <h0tcatnohotmensh0tcat@gmail.com>
2022-05-20 11:14:43 +0900

    Mainクラス以外でも画面遷移を可能にする

--- core/src/h0tcat/game/towa/Main.java
+++ core/src/h0tcat/game/towa/Main.java
@@ -1,36 +1,18 @@
 package h0tcat.game.towa;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Screen;
-
-import java.util.HashMap;
 
 import h0tcat.game.towa.ui.Title;
 
 public class Main extends Game {
-	private static HashMap<String, Screen> scenes;
 
 	@Override
 	public void create () {
-		scenes = new HashMap<>();
-		
-		scenes.put("title", new Title());
-		setScreen(getScene("title"));
-	}
-
-	public static Screen getScene(String scene){
-		return scenes.get(scene);
+		setScreen(new Title(this));
 	}
 
 	@Override
 	public void render () {
 		super.render();
-	}
-	
-	@Override
-	public void dispose () {
-		for(String keys : scenes.keySet()){
-			scenes.get(keys).dispose();
-		}
 	}
 }
